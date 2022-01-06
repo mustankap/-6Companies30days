dc61368fbf701b87b1bbdeae4f55366f3de60e7d
Mustansir Kapasi <44290089+mustankap@users.noreply.github.com>
2022-01-06 17:38:45 +0530

    dp solution added

--- GMS/ugly.java
+++ GMS/ugly.java
@@ -1,8 +1,40 @@
-//This solution gave RUNTIME Error
+//https://practice.geeksforgeeks.org/problems/ugly-numbers2254/1/#
 class Solution {
     /* Function to get the nth ugly number*/
     long getNthUglyNo(int n) {
         // code here
+        long[] ugly = new long[n];
+        ugly[0]=1;
+        int i2=0,i3=0,i5=0;long next=1;
+        long nexti2=ugly[i2]*2;
+        long nexti3=ugly[i3]*3;
+        long nexti5=ugly[i5]*5;
+        
+        
+        for(int i=1;i<n;i++)
+        {
+            
+            next=Math.min(nexti2,Math.min(nexti5,nexti3));
+            ugly[i]=next;
+            if(next==nexti2)
+            ++i2;nexti2=ugly[i2]*2;
+            if(next==nexti3)
+            ++i3;nexti3=ugly[i3]*3;
+            if(next==nexti5)
+            ++i5;nexti5=ugly[i5]*5;
+        }
+          return next;
+    }
+}
+
+/**
+
+This solution gave RUNTIME Error
+
+class Solution {
+    Function to get the nth ugly number
+    long getNthUglyNo(int n) {
+         
         long no=0;
         long i =1;
         int count=1;
@@ -28,3 +60,4 @@ class Solution {
           return a;
     }
 }
+**/
